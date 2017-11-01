d626f65f55ca5fcb9a8c2b4706cd6e378e4871fd
isabel-thx <isabelhx.teh@gmail.com>
2017-11-01 18:01:20 +0800

    user able to find listing based on location, period of stay, and number of guests

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,10 +2,17 @@ class HelloController < ApplicationController
 
 	def index
 
-		# @listings = Listing.all
-		@listings =Listing.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+		# @listings = Listing.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+
+			if params[:location]
+				@listings = Listing.search(location: params[:location], guest: params[:guest], start_date: params[:start_date], end_date: params[:end_date]).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+	  	else
+	    	@listings = Listing.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+	  	end
+
+	end
 		#render :index, layout: false
 		#"layout: false" if you want to not include application.html.erb
-	end
+
 
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,5 +6,12 @@ class Listing < ApplicationRecord
   validates :num_of_rooms, presence: true, numericality: { greater_than: 0, only_integer: true }
   validates :num_of_bathrooms, presence: true, numericality: { greater_than: 0, only_integer: true }
   validates :price, presence: true, numericality: { greater_than: 0, only_integer: true }
+  validates :max_num_of_guests, presence: true, numericality: { greater_than: 0, only_integer: true }
 
+
+
+	def self.search(location:, guest:, start_date:, end_date:)
+		Listing.where("(SELECT COUNT(*) FROM reservations WHERE (reservations.listing_id = listings.id) AND (reservations.start_date, reservations.end_date) OVERLAPS (DATE ?, DATE ?)) = 0", start_date, end_date).where("(state LIKE ? OR city LIKE ? OR country LIKE ?)", "%#{location}%", "%#{location}%", "%#{location}%").where("(max_num_of_guests >= ?)", guest)
+	end
+	
 end
