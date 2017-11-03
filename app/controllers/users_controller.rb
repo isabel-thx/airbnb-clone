3df0f3ce21c21654004cccf6052ed5912e7d2f22
isabel-thx <isabelhx.teh@gmail.com>
2017-11-03 16:49:48 +0800

    change user role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update, :upgrade]
+  before_action :set_user, only: [:show, :edit, :update, :upgrade, :downgrade]
 
   def show
     # @user = User.find(params[:id])
